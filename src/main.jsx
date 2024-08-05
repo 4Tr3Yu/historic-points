b130aa5915b0da8502b7823154fe8c2d04d01d16
Jo <jomirandav@hotmail.com>
2024-08-05 18:13:54 -0400

    tailwind added, not working

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1 className="text-3xl font-bold underline"> Historic Points</h1>
   </React.StrictMode>,
 )
